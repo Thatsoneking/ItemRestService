d985d20c7dcf047c94d27710733d92a8eb219520
Boris <borisvereb@gmail.com>
2019-09-30 09:22:34 +0200

    Implementing ItemLibrary , Controler

---
+++ ItemLibrary/Model/Item.cs
@@ -0,0 +1,30 @@
+﻿using DocumentFormat.OpenXml.Office2013.PowerPoint.Roaming;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItemLibrary.Model
+{
+    public class Item
+    {
+        public int Id;
+        public string Name;
+        public string Quality;
+        public double Quantity;
+
+        void Data() {}
+
+        public Item(int id, string name, string quality, double quantity)
+        {
+            Id = id;
+            Name = name;
+            Quality = quality;
+            Quantity = quantity;
+        }
+        public override string ToString()
+        {
+            return Id+Name+Quality+Quantity;
+        }
+
+    }
+}
